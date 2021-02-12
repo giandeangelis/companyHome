355bfcc5188587a6c7349e702327ca375b8173d6
giandeangelis <deangelisgianluca.g@gmail.com>
2021-02-12 15:49:05 +0000

    Make navbar responsive. Hamburger and cross icons to be fixed

---
+++ resources/script.js
@@ -0,0 +1,14 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('navbar');
+const navItems = document.getElementById('nav-items');
+const ezyIcon = document.getElementById('logo-small');
+
+const openNav = () => {
+    hamburger.style.display = 'none';
+    ezyIcon.style.display = 'none';
+    nav.style.height = '25%';
+    nav.style.flexFlow = 'column';
+    navItems.style.display = 'flex';
+};
+
+hamburger.addEventListener('click', openNav);
