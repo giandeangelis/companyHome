fd9ebcffc944d38f36cf98e26ec3d6fff808a9f2
giandeangelis <deangelisgianluca.g@gmail.com>
2021-02-13 15:09:20 +0000

    Implement expanding navbar for mobile version

--- resources/script.js
+++ resources/script.js
@@ -1,16 +1,10 @@
 //Handle the opening of the nav menu on mobile version
 
-const hamburger = document.getElementById('hamburger');
+const hamburger = document.getElementById('hamburger-container');
 const nav = document.getElementById('navbar');
-const navItems = document.getElementById('nav-items');
-const ezyIcon = document.getElementById('logo-small');
 
 const openNav = () => {
-    hamburger.style.display = 'none';
-    ezyIcon.style.display = 'none';
-    nav.style.height = '25%';
-    nav.style.flexFlow = 'column';
-    navItems.style.display = 'flex';
+    nav.classList.toggle('change');
 };
 
 hamburger.addEventListener('click', openNav);
